c715b29cf3c6d75e61f0dde9fb9c5859b80ed924
RedOWL <vda.perm@mail.ru>
2019-06-23 17:39:04 +0500

    Я сразу смазал карту будня,

--- Task10/Program.cs
+++ Task10/Program.cs
@@ -9,11 +9,11 @@ namespace Task10
         {
             Console.WriteLine("Метод стягивания вершин");
             // Добавляем основные вершины
-            Vertex.VList.Add(new Vertex(0, new List<int> { 1, 3 } )); // 0
-            Vertex.VList.Add(new Vertex(6, new List<int> { 4 } )); // 1
-            Vertex.VList.Add(new Vertex(7, new List<int> { 0 } )); // 2
-            Vertex.VList.Add(new Vertex(7, new List<int> { 2, 4 } )); // 3
-            Vertex.VList.Add(new Vertex(7, new List<int> { 2 } )); // 4
+            Vertex.VList.Add(new Vertex(0, new List<int> { 1, 3 } ));   // 0
+            Vertex.VList.Add(new Vertex(6, new List<int> { 4 } ));      // 1
+            Vertex.VList.Add(new Vertex(7, new List<int> { 0 } ));      // 2
+            Vertex.VList.Add(new Vertex(7, new List<int> { 2, 4 } ));   // 3
+            Vertex.VList.Add(new Vertex(7, new List<int> { 2 } ));      // 4
             // Добавляем дополнительную вершину - остров для демонстрации стягивания
             // Vertex.VList.Add(new Vertex(9, new List<int> { })); // 5
             // Вызываем метод стягивания
